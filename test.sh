fa73472f10a24326faccafe09aaec08f55259bc4
Furqan Imtiaz <furqan.imtiaz@yahoo.com>
2025-07-31 22:28:29 -0500

    Comments addition

--- test.sh
+++ test.sh
@@ -2,12 +2,16 @@
 
 age=$1
 name=$2
-echo "age you entered is $1"
-echo "$twitter"
+
+echo "age you entered is $1" #caling age which we set as $1
+echo "$twitter" #Env variable set in ~/.bashrc then source ~/.bashrc to save the variable
+
 echo "Test"
 sleep 3
-echo "Name is $2"
+echo "Name is $2" #calling name which we set as $2
 echo "Test complete"
 sleep 3
 echo "End"
 
+
+#for running the script we do ./test.sh 23 furq which assigns 23 as $1 and furq as $2
