b0d1d5d6de6c36e40975fc36cfee21b78469db3d
Furqan Imtiaz <furqan.imtiaz@yahoo.com>
2025-08-01 22:42:45 -0500

    Comments

--- name.sh
+++ name.sh
@@ -14,3 +14,10 @@ sleep 1
 echo "You have the best $compliment I've ever seen $name"
 sleep 2
 echo "You are currently logged in as $user and in the dir $whereami. Today is $date"
+
+
+#Prac
+#name=$1
+#date=$2
+#whereami=$(whoami)
+
